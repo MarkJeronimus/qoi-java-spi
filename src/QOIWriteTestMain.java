4847988d11bc61878b859c775bea0ee5e24f694f
MarkJeronimus <mark.jeronimus@gmail.com>
2022-05-20 17:16:09 +0200

    Improve write test

--- src/QOIWriteTestMain.java
+++ src/QOIWriteTestMain.java
@@ -5,17 +5,21 @@ import java.awt.Graphics;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import javax.imageio.ImageIO;
 import javax.imageio.spi.IIORegistry;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 import javax.swing.WindowConstants;
@@ -33,7 +37,7 @@ import org.digitalmodular.qoi.QOIImageWriterSpi;
 public class QOIWriteTestMain extends JPanel {
 	@SuppressWarnings({"StaticVariableMayNotBeInitialized", "StaticCollection"})
 	private static List<Path> files;
-	private        int        fileIndex = 4;
+	private static int        fileIndex = 0;
 
 	private BufferedImage image1 = null;
 	private BufferedImage image2 = null;
@@ -41,10 +45,12 @@ public class QOIWriteTestMain extends JPanel {
 	public static void main(String... args) throws IOException {
 		IIORegistry.getDefaultInstance().registerServiceProvider(new QOIImageReaderSpi());
 		IIORegistry.getDefaultInstance().registerServiceProvider(new QOIImageWriterSpi());
+
 		// QOI Encoding business is in convertImage()
 
 //		Path dir = Paths.get("pngsuite/");
-		Path dir = Paths.get("qoi_test_images/");
+//		Path dir = Paths.get("qoi_test_images/");
+		Path dir = Paths.get("testsuite/");
 
 		try (Stream<Path> stream = Files.list(dir)) {
 			files = stream.filter(file -> !Files.isDirectory(file))
@@ -57,6 +63,15 @@ public class QOIWriteTestMain extends JPanel {
 			System.err.println("No files found in " + dir.toAbsolutePath());
 			System.exit(1);
 		}
+
+		fileIndex = ThreadLocalRandom.current().nextInt(files.size());
+//		fileIndex = IntStream.range(0, files.size())
+//		                     .filter(i -> {
+//			                     Path file = files.get(i);
+//			                     return file.getFileName().toString().startsWith("E78DD866");
+//		                     })
+//		                     .findFirst()
+//		                     .orElse(0);
 
 		SwingUtilities.invokeLater(() -> {
 			JFrame f = new JFrame();
@@ -79,10 +94,9 @@ public class QOIWriteTestMain extends JPanel {
 		convertImage();
 		assert image1 != null;
 
-		setPreferredSize(new Dimension(image1.getWidth() * 2, image1.getHeight()));
+		setPreferredSize(new Dimension(512, 256));
 
 		// Yes I know it's bad practice to do work on the EDT, but it's a cheap test class anyway.
-		SwingUtilities.invokeLater(this::convertImage);
 
 		addMouseListener(new MouseAdapter() {
 			@Override
@@ -102,18 +116,28 @@ public class QOIWriteTestMain extends JPanel {
 	}
 
 	private void convertImage() {
+		String format   = "qoi";
 		Path   file     = files.get(fileIndex);
 		String filename = file.getFileName().toString();
-		File   qoiFile  = file.getParent().resolve(filename.substring(0, filename.length() - 4) + ".qoi1").toFile();
+		filename = filename.substring(0, filename.length() - 4) + '.' + format;
+		File qoiFile = file.getParent().resolve(filename).toFile();
 
 		try {
-			image1 = ImageIO.read(file.toFile());  // PNG
-			ImageIO.write(image1, "qoi", qoiFile); // QOI
-			image2 = ImageIO.read(qoiFile);        // QOI
+			image1 = ImageIO.read(file.toFile());              // PNG
+			ImageIO.write(image1, format, qoiFile); // QOI
+			image2 = ImageIO.read(qoiFile);                    // QOI
 		} catch (IOException ex) {
 			ex.printStackTrace();
 			System.exit(1);
 		}
+
+		byte[] pixels1 = ((DataBufferByte)image1.getRaster().getDataBuffer()).getData();
+		byte[] pixels2 = ((DataBufferByte)image2.getRaster().getDataBuffer()).getData();
+		if (!Arrays.equals(pixels1, pixels2))
+			SwingUtilities.invokeLater(() -> JOptionPane.showMessageDialog(this,
+			                                                               "Images differ!",
+			                                                               getClass().getSimpleName(),
+			                                                               JOptionPane.ERROR_MESSAGE));
 
 		Frame topLevelAncestor = (Frame)getTopLevelAncestor();
 		if (topLevelAncestor != null) {
@@ -130,12 +154,18 @@ public class QOIWriteTestMain extends JPanel {
 
 		drawTransparencyCheckerboard(g);
 
-		g.drawImage(image1, 0, 0, null);
-		g.drawImage(image2, image1.getWidth(), 0, null);
+		g.drawImage(image1,
+		            0, 0, getWidth() / 2, getHeight(),
+		            0, 0, image1.getWidth(), image1.getHeight(),
+		            null);
+		g.drawImage(image2,
+		            getWidth() / 2, 0, getWidth(), getHeight(),
+		            0, 0, image2.getWidth(), image2.getHeight(),
+		            null);
 	}
 
 	private void drawTransparencyCheckerboard(Graphics g) {
-		int blockSize = 4;
+		int blockSize = 16;
 		for (int y = 0; y < getWidth(); y += blockSize) {
 			boolean b = (y / blockSize & 1) != 0;
 			for (int x = 0; x < getWidth(); x += blockSize) {
